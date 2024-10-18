2b51300f7b2e54f022d900b7d2439e3c4ab4668c
shekar <null>
2024-10-18 15:39:38 +0530

    lists and tuples

---
+++ Python/ex3.py
@@ -0,0 +1,25 @@
+# ask the user to enter their three favourite movie and enter in list
+# movies = []
+# movies.append(input("Enter first movie: "))
+# movies.append(input("Enter second movie: "))
+# movies.append(input("Enter third movie: "))
+# print(movies)
+
+# check if list is palindrome or not
+palindrome = ["m","a","a","m"]
+palindrome2 = palindrome.copy()
+palindrome2.reverse()
+if palindrome == palindrome2:
+    print("palindrome")
+else:
+    print("not palindrome")
+
+
+#cont the number of student with the "A" grade in the following tuple 
+grades = ("C","D","A","A","B","B","A")
+print(grades.count("A"))
+
+#store the above value into a list and sort
+new_list = list(grades)
+new_list.sort()
+print(new_list)
---
+++ Python/tuple.py
@@ -0,0 +1,11 @@
+# tuple
+tup = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(type(tup))
+print(tup[1])
+
+# tuple method
+tup2 = (0, 1, 2, 1, 3, 4, 5, 6, 7, 8, 9)
+# tuple count
+print(tup2.count(1))
+# tuple index
+print(tup2.index(3))
